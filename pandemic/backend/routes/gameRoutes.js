10a091e036ba96764231e13a8947f7e6e75b44a8
Leonardo Calvet <leonardofmcalvet@gmail.com>
2025-07-01 00:15:24 -0300

    feat: Reestruturação da estrutura dos arquivos e implementação da gestão de cartas e controle da rebelião

--- pandemic/backend/routes/gameRoutes.js
+++ pandemic/backend/routes/gameRoutes.js
@@ -1,15 +1,17 @@
 const express = require('express');
+const express = require('express');
 const router = express.Router();
 const { getGameState, handleAction } = require('../controllers/gameController');
-const initGame = require('../game/initGame');
+const initGame = require('../gameLogic/initGame');
 
 router.get('/state', getGameState);
 router.post('/action', handleAction);
 
 router.post('/start', (req, res) => {
-  const { players } = req.body;
-  req.app.locals.gameState = initGame(players);
-  res.status(200).json({ message: 'Jogo iniciado' });
+    const { players } = req.body;
+    // A chamada da função initGame permanece a mesma
+    req.app.locals.gameState = initGame(players);
+    res.status(200).json({ message: 'Jogo iniciado' });
 });
 
 module.exports = router;
